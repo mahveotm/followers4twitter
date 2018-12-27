ff29ecf2cfc340ee7d6f2f0a2d4890ee363271a6
Osuolale Marvellous <31046066+mahveotm@users.noreply.github.com>
2018-12-27 17:34:28 +0100

    Add files via upload

---
+++ retweet.py
@@ -0,0 +1,48 @@
+import tweepy, time
+# this script enables you to retweet your timeline feeds. Several error including attempts to retweet already retweeted tweets are taken into account
+
+CONSUMER_KEY ='your consumer key goes here'
+CONSUMER_SECRET ='your consumer secret goes here'
+ACCESS_KEY ='your access key goes here'
+ACCESS_SECRET = 'your access secret goes here'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+FILE_NAME = 'last_seen_id.txt'
+#You need to create an empty text file and name last_seen_id.txt This script will read the tweet id and keep updating it when done to avoid repeating a tweet
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def retweet():
+    print('retrieving and attempting to retweet...')
+ 
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    
+    home = api.home_timeline(
+                        last_seen_id,)
+    for status in reversed(home):
+        print(str(status.id)+status.text)
+        last_seen_id = status.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        print('found tweets')
+        api.retweet(status.id)
+
+while True:
+	try:
+		retweet()
+		time.sleep(30)
+	except:
+		print('some error with the tweet, would retry again')
+		time.sleep(30)
